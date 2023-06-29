1c26b1ac2716eac7cf1d611d69236d8fff1c38fb
yunji jo <dbtmxlsk2007@naver.com>
2023-06-29 19:16:12 +0900

    update : athena dynamic dag

--- dags/athena_movie_genre.py
+++ dags/athena_movie_genre.py
@@ -12,17 +12,17 @@ default_args = {
     "retry_delay": timedelta(minutes=1)
 }
 
-with DAG("athena_{{ dag_id }}", default_args=default_args, schedule_interval= '@once') as dag:
+with DAG("athena_movie_genre", default_args=default_args, schedule_interval= '@once') as dag:
 
     insert = AthenaOperator(
         task_id='insert',
         database='athena_db',
-        query='''INSERT INTO "athena_db"."{{ table }}"
+        query='''INSERT INTO "athena_db"."movie_genre"
 SELECT DISTINCT t.*
-FROM "athena_db"."{{ temp_table }}" t
-LEFT JOIN "athena_db"."{{ table }}" s ON t.movieCd = s.movieCd
+FROM "athena_db"."movie_genre_temp" t
+LEFT JOIN "athena_db"."movie_genre" s ON t.movieCd = s.movieCd
 WHERE s.movieCd IS NULL;''',
-        output_location='s3://team3-athena-results/accumulated_data/{{ table }}/query_results/',
+        output_location='s3://team3-athena-results/accumulated_data/movie_genre/query_results/',
         aws_conn_id='aws_conn_id'
     )
 
@@ -30,11 +30,10 @@ WHERE s.movieCd IS NULL;''',
     save = AthenaOperator(
         task_id='save_table_to_s3',
         database='athana_db',
-        query='SELECT * FROM  "athena_db"."{{ table }}";',
-        output_location='s3://team3-athena-results/accumulated_data/{{ table }}/{{ table }}_result/',
+        query='SELECT * FROM  "athena_db"."movie_genre";',
+        output_location='s3://team3-athena-results/accumulated_data/movie_genre/movie_genre_result/',
         aws_conn_id='aws_conn_id'
     ) 
     
     insert >> save 
-
 
--- dags/athena_movie_summary.py
+++ dags/athena_movie_summary.py
@@ -12,17 +12,17 @@ default_args = {
     "retry_delay": timedelta(minutes=1)
 }
 
-with DAG("athena_{{ dag_id }}", default_args=default_args, schedule_interval= '@once') as dag:
+with DAG("athena_movie_summary", default_args=default_args, schedule_interval= '@once') as dag:
 
     insert = AthenaOperator(
         task_id='insert',
         database='athena_db',
-        query='''INSERT INTO "athena_db"."{{ table }}"
+        query='''INSERT INTO "athena_db"."movie_summary"
 SELECT DISTINCT t.*
-FROM "athena_db"."{{ temp_table }}" t
-LEFT JOIN "athena_db"."{{ table }}" s ON t.movieCd = s.movieCd
+FROM "athena_db"."movie_summary_temp" t
+LEFT JOIN "athena_db"."movie_summary" s ON t.movieCd = s.movieCd
 WHERE s.movieCd IS NULL;''',
-        output_location='s3://team3-athena-results/accumulated_data/{{ table }}/query_results/',
+        output_location='s3://team3-athena-results/accumulated_data/movie_summary/query_results/',
         aws_conn_id='aws_conn_id'
     )
 
@@ -30,11 +30,10 @@ WHERE s.movieCd IS NULL;''',
     save = AthenaOperator(
         task_id='save_table_to_s3',
         database='athana_db',
-        query='SELECT * FROM  "athena_db"."{{ table }}";',
-        output_location='s3://team3-athena-results/accumulated_data/{{ table }}/{{ table }}_result/',
+        query='SELECT * FROM  "athena_db"."movie_summary";',
+        output_location='s3://team3-athena-results/accumulated_data/movie_summary/movie_summary_result/',
         aws_conn_id='aws_conn_id'
     ) 
     
     insert >> save 
-
 
