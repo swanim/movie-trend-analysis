69fc60e1f5286a7d2eec1a0db06244c05b75265d
YUNJI <49296139+joyunji@users.noreply.github.com>
2023-07-03 02:22:51 +0900

    Update create_table.sql
    - table별 location 수정
    - movie_sales_pred 테이블 추기

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,6 +1,6 @@
 /*
 Create table
-Athena SQL을 사용하여 s3에 daily로 업데이트되는 csv파일을 원본으로 테이블 생성
+Athena를 사용하여 s3에 daily로 업데이트되는 csv파일을 원본으로 테이블 생성
 - movie_codes
 - movie_company
 - movie_director
@@ -8,6 +8,7 @@ Athena SQL을 사용하여 s3에 daily로 업데이트되는 csv파일을 원본
 - movie_grade
 - movie_sales
 - movie_summary
+- movie_sales_pred
 */
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_codes` (
@@ -17,7 +18,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_codes` (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_codes/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_codes/movie_codes_table'
 TBLPROPERTIES (
   'classification' = 'csv',
   'skip.header.line.count' = '1'
@@ -30,7 +31,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_director` (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_director/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_director/movie_director_table'
 TBLPROPERTIES (
   'classification' = 'csv',
   'skip.header.line.count' = '1'
@@ -43,7 +44,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_company` (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_company/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_company/movie_company_table'
 TBLPROPERTIES (
   'classification' = 'csv',
   'skip.header.line.count' = '1'
@@ -54,7 +55,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_grade` ( `movieCd` varcha
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_grade/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_grade/movie_grade_table'
 TBLPROPERTIES ( 'classification' = 'csv', 'skip.header.line.count' = '1'
 );
 
@@ -63,7 +64,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_genre` ( `movieCd` varcha
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_genre/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_genre/movie_genre_table'
 TBLPROPERTIES ( 'classification' = 'csv', 'skip.header.line.count' = '1'
 );
 
@@ -72,7 +73,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_summary` ( `movieCd` int,
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_summary/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_summary/movie_summary_table'
 TBLPROPERTIES ( 'classification' = 'csv', 'skip.header.line.count' = '1'
 )
 
@@ -99,10 +100,59 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `athena_db`.`movie_sales` (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES ('field.delim' = ',')
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://team3-project3-bucket/daily/movie_sales/'
+LOCATION 's3://team3-athena-results/accumulated_data/movie_sales/movie_sales_table'
 TBLPROPERTIES (
   'classification' = 'csv',
   'skip.header.line.count' = '1'
 );
 -- movie_sales에 showingDays 칼럼 추가
 ALTER TABLE movie_sales ADD COLUMNS (showingDays FLOAT);
+
+
+CREATE EXTERNAL TABLE `movie_sales_pred`(
+  `date` bigint, 
+  `rank` bigint, 
+  `rankinten` bigint, 
+  `rankoldandnew` string, 
+  `moviecd` bigint, 
+  `movienm` string, 
+  `opendt` string, 
+  `salesamt` bigint, 
+  `salesshare` double, 
+  `salesinten` bigint, 
+  `saleschange` double, 
+  `salesacc` bigint, 
+  `audicnt` bigint, 
+  `audiinten` bigint, 
+  `audichange` double, 
+  `audiacc` bigint, 
+  `scrncnt` bigint, 
+  `showcnt` bigint, 
+  `salespred` bigint)
+ROW FORMAT DELIMITED 
+  FIELDS TERMINATED BY ',' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://team3-athena-results/accumulated_data/movie_sales_pred/'
+TBLPROPERTIES (
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='movie_sales_pred', 
+  'areColumnsQuoted'='false', 
+  'averageRecordSize'='152', 
+  'classification'='csv', 
+  'columnsOrdered'='true', 
+  'compressionType'='none', 
+  'delimiter'=',', 
+  'objectCount'='1', 
+  'recordCount'='2', 
+  'sizeKey'='331', 
+  'skip.header.line.count'='1', 
+  'typeOfData'='file')
+
+
+
+
