0c98074969fb9a2a6c31ab29d857cd9baf63c4c7
YUNJI <49296139+joyunji@users.noreply.github.com>
2023-07-03 02:24:31 +0900

    Update create_temp_table.sql
    movie_sales_pred_temp 테이블 추가

--- sql/create_temp_table.sql
+++ sql/create_temp_table.sql
@@ -8,6 +8,7 @@ Atherna를 사용하여 매일 업데이트 되는 csv 파일을 임시로 저�
 - movie_grade_temp
 - movie_sales_temp
 - movie_summary_temp
+- movie_sales_pred_temp
 */
 CREATE EXTERNAL TABLE IF NOT EXISTS movie_codes_temp(
     `movieCd` varchar(10),
@@ -68,3 +69,48 @@ LOCATION 's3://team3-project3-bucket/daily/temp/movie_sales_temp'
 
 -- movie_sales_temp에 showingDays 칼럼 추가
 ALTER TABLE movie_sales_temp ADD COLUMNS (showingDays FLOAT);
+
+
+CREATE EXTERNAL TABLE `movie_sales_pred_00f8fd0f4f6feb0efbaebc50c4180664`(
+  `date` bigint, 
+  `rank` bigint, 
+  `rankinten` bigint, 
+  `rankoldandnew` string, 
+  `moviecd` bigint, 
+  `movienm` string, 
+  `opendt` string, 
+  `salesamt` bigint, 
+  `salesshare` double, 
+  `salesinten` bigint, 
+  `saleschange` double, 
+  `salesacc` bigint, 
+  `audicnt` bigint, 
+  `audiinten` bigint, 
+  `audichange` double, 
+  `audiacc` bigint, 
+  `scrncnt` bigint, 
+  `showcnt` bigint, 
+  `salespred` bigint)
+ROW FORMAT DELIMITED 
+  FIELDS TERMINATED BY ',' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://team3-project3-bucket/daily/temp/movie_sales_pred/'
+TBLPROPERTIES (
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='movie_sales_pred_temp', 
+  'areColumnsQuoted'='false', 
+  'averageRecordSize'='147', 
+  'classification'='csv', 
+  'columnsOrdered'='true', 
+  'compressionType'='none', 
+  'delimiter'=',', 
+  'objectCount'='1', 
+  'recordCount'='20', 
+  'sizeKey'='2967', 
+  'skip.header.line.count'='1', 
+  'typeOfData'='file')
